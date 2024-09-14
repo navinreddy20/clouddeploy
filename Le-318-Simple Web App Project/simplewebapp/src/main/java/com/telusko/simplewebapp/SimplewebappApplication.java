be8e60d1379fc8b2171db9effbc80325677fa3db
Gaurav Sharma <gsharma1154@gmail.com>
2024-09-15 00:04:09 +0530

    initial commit for cloud

---
+++ Le-318-Simple Web App Project/simplewebapp/src/main/java/com/telusko/simplewebapp/SimplewebappApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.simplewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplewebappApplication.class, args);
+	}
+
+}
